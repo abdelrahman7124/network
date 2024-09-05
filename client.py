c527f498b5ae4bc11f724115059483da7a81d8fc
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-09-05 06:46:59 +0300

    modified the client to send the packet number and check if the received one matches

--- client.py
+++ client.py
@@ -2,17 +2,29 @@ import socket as sk
 
 ip='192.168.1.15'
 port =12345
+addr = (ip,port)
+packet_num = 0
 
 client_socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+
 while True:
+    print(f"packet number {packet_num}")
     message = input("Enter the message you want to send [enter ""exit"" to exit]")
-    byte_message = message.encode()  
-    client_socket.sendto(byte_message, (ip, port))
-    if message == "exit":
+    sent_message = str(packet_num) + ":" + message
+    byte = str(sent_message).encode()
+    
+    client_socket.sendto(byte,addr)
+    if message.lower() == "exit":
         break
     data, server = client_socket.recvfrom(1024)
     received_message = data.decode() 
-    print(f"Received message from server: {received_message}")
+    
+    if received_message.lower() == f"{packet_num}":
+        packet_num += 1
+        print("sent message to server succesfully")
+    else :
+        print("couldn't send message")
     
 
 client_socket.close()
+
