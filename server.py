c56219a5728ea220ff5c89f75f26543be0933c42
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-09-05 06:46:06 +0300

    modified the server to check if the given packet number matches

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket as sk
+
 host = sk.gethostbyname(sk.gethostname())
 port = 12345
+
+packet_num = 0
 
 server_socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 server_socket.bind((host, port))
     
-print(f"Server started on {host}:{port}, waiting for messages...")
     
 while True:
     data, addr = server_socket.recvfrom(1024) 
-    received_message = data.decode() 
-    print(f"Received message: {received_message} from {addr}")
-    if received_message == "exit":
-        break
-    response = "Message received"
-    byte_response = response.encode() 
-    server_socket.sendto(byte_response, addr)
+    message = data.decode()
+    received_packet_num , received_message = message.split(":")
+    if received_message.lower() == "exit":
+        break  
+    if received_packet_num == str(packet_num): 
+        byte = f"{packet_num}".encode()
+        server_socket.sendto(byte,addr)
+        packet_num +=1
+        print(f"Received {received_message} from {addr}")
+    else :
+        server_socket.sendto("-1".encode(),addr)
+        print ("Message interrupted")
     
 server_socket.close()
